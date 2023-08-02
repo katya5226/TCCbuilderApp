a9984cfcb6bc6a37852007d1fa21839c7451c2df
katya5226 <47118494+katya5226@users.noreply.github.com>
2023-08-02 09:07:07 +0000

    Moved enums out of cirsim

---
+++ src/com/lushprojects/circuitjs1/client/HeatSimProps.java
@@ -0,0 +1,18 @@
+package com.lushprojects.circuitjs1.client;
+
+public class HeatSimProps {
+
+    public enum Property {  // TODO: implement in code
+        DENSITY,
+        HEATCAPACITY,
+        THCONDUCTIVITY,
+        EMISSIVITY
+    }
+
+    public enum BC {
+        ADIABATIC,
+        CONSTHEATFLUX,
+        CONSTTEMP,
+        CONVECTIVE
+    }
+}
